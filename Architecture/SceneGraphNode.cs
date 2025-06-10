c75a22a17862c92a40363563b1e0dcf282dea2c0
Motrix235 <maxjbbrouwer@gmail.com>
2025-06-10 11:13:07 +0200

    Scene Graph Node class

---
+++ Architecture/SceneGraphNode.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Mathematics;
+using Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFOGR2025TemplateP2.Architecture
+{
+    internal class SceneGraphNode
+    {
+        Matrix4 objectToParent;
+        Mesh mesh;
+        List<SceneGraphNode> children;
+
+        void Render()
+        {
+
+        }
+    }
+}
